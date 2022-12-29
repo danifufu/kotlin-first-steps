31cd7d926f490003955021a6a2cb8e94dd84740c
Daniela Araujo <araujo.daniela@outlook.com>
2022-12-29 14:42:32 -0300

    Testing comment types in Kotlin

---
+++ kotlin-codes/src/main/kotlin/com/example/comments/main.kt
@@ -0,0 +1,32 @@
+package com.example.comments
+
+fun main() {
+
+    // single-line comment
+
+
+    /*
+    comment with
+    more than one line
+     */
+
+
+    /**
+     * comment for function and class documentation
+     */
+
+
+    /*
+
+    comment within a comment!
+
+    // hello!
+
+    /*
+    hi!
+    how are you?
+    */
+
+    */
+
+}
