0b58bbb680377f6a4eb4c2d3e383caf2c4f6af35
Daniela Araujo <araujo.daniela@outlook.com>
2023-01-09 15:14:18 -0300

    Testing the For expression in Kotlin

---
+++ kotlin-codes/src/main/kotlin/com/example/forexpression/main.kt
@@ -0,0 +1,25 @@
+package com.example.forexpression
+
+fun main() {
+
+    // increment from 1 to 1
+    for (i in 0..5) {
+        println("Counter i = $i")
+    }
+
+    // increment from 2 to 2
+    for (i in 0..10 step 2) {
+        println("Counter i = $i")
+    }
+
+    // decrement from 1 to 1
+    for (j in 5 downTo 0) {
+        println("Counter j = $j")
+    }
+
+    // decrement from 2 to 2
+    for (j in 10 downTo 0 step 2) {
+        println("Counter j = $j")
+    }
+
+}
